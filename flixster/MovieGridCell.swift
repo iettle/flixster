213b8c910b8c4aa7b27bd055d5404ab052781461
Juliette Rike <juliette.rike98@gmail.com>
2019-10-25 00:49:57 -0400

    collection view buggy

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Juliette Rike on 10/25/19.
+//  Copyright © 2019 Juliette Rike. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
