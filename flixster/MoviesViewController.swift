9ccd10ad1149af22e6d8f3c4a86f413a3778c01f
Juliette Rike <juliette.rike98@gmail.com>
2019-10-24 23:56:07 -0400

    added nav and passed data

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -72,14 +72,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // sender is cell that was tapped on
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        // Briefly highlights cell when clicked
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
